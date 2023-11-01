be469712399a135e01f5fc231e636f8782415a0c
topbusiness <1010>
2023-11-01 10:57:02 +0200

    hh

--- lib/core/utils/app_strings.dart
+++ lib/core/utils/app_strings.dart
@@ -1,7 +1,7 @@
 class AppStrings{
   static const String fontFamily = "Cairo";
   static const String noRouteFound = "No Route Found";
-  static const String appName = "app_name";
+  static const String appName = "topsale";
   static const String locale = "locale";
   static const String contentType = "Content-Type";
   static const String applicationJson = "application/json";
