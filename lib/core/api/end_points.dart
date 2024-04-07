a2d1ca09a93fc24a93cbcd1f0febda20468abb57
ahmedelsapagh <TopBusiness110>
2024-04-07 09:57:54 +0200

    doen 20

--- lib/core/api/end_points.dart
+++ lib/core/api/end_points.dart
@@ -6,4 +6,5 @@ class EndPoints {
   static const String allCategoriesUrl = "$baseUrl/api/product.category/";
   static const String allProducts =
       '$baseUrl/api/product.product/?query={id,name,qty_available,list_price,currency_id,uom_name,uom_id,categ_id,image_1920}&filter=[["sale_ok", "=", "True"]]';
+  static const String allCategoryProducts = '$baseUrl/api/product.product/';
 }
